4aedb8d029a6eaf06374189f3061fa939ec365e6
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-08-11 00:06:58 +0600

    litttle bit indenting name issue  fixing

--- users/validators.py
+++ users/validators.py
@@ -1,4 +1,8 @@
 from django.core.validators import RegexValidator
+
+
+
+
 bd_phone_number_validator = RegexValidator(
     regex=r'^\+8801\d{9}$',
     message="Phone number must be entered in the format: +8801234567890. Up to 15 digits allowed.",
