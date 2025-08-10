dc51aad521a1ab808b3a6d5519fc84488f50d5cb
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-08-11 00:30:22 +0600

    memebrship  api endpoint is working with custom exception handler and custom api response

--- dumbbell_don/settings.py
+++ dumbbell_don/settings.py
@@ -166,7 +166,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "COERCE_DECIMAL_TO_STRING": False,
-    # "EXCEPTION_HANDLER": "core.utils.custom_exception_handler.custom_exception_handler",
+    "EXCEPTION_HANDLER": "core.utils.custom_exception_handler.custom_exception_handler",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
