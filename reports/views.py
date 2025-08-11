ff8787e25a3d090706560d0aeb8359eccf9694fb
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-08-11 09:31:16 +0600

    role creation done

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 from .models import BaseReport, MembershipReport, AttendanceReport, FeedbackReport
 from .serializers import BaseReportSerializer, MembershipReportSerializer, AttendanceReportSerializer, FeedbackReportSerializer
@@ -6,12 +7,21 @@ from core.utils.BASEModelViewSet import BaseModelViewSet
 class MembershipReportViewSet(BaseModelViewSet):
     queryset = MembershipReport.objects.all()
     serializer_class = MembershipReportSerializer
+    permission_classes = [IsAdminUser]
+    def perform_create(self, serializer):
+        serializer.save(generated_by=self.request.user)
 
 class AttendanceReportViewSet(BaseModelViewSet):
     queryset = AttendanceReport.objects.all()
     serializer_class = AttendanceReportSerializer
+    permission_classes = [IsAdminUser]
+    def perform_create(self, serializer):
+        serializer.save(generated_by=self.request.user)
 
 class FeedbackReportViewSet(BaseModelViewSet):
     queryset = FeedbackReport.objects.all()
     serializer_class = FeedbackReportSerializer
+    permission_classes = [IsAdminUser]
+    def perform_create(self, serializer):
+        serializer.save(generated_by=self.request.user)
 
