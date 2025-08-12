693693e57c3517e825daf8816c262e3b48e91b23
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-08-12 22:48:21 +0600

    error handler refactoring

--- dumbbell_don/urls.py
+++ dumbbell_don/urls.py
@@ -25,6 +25,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 import re
+from core.custom_basic_error_handler import custom_handler400, custom_handler403, custom_handler404, custom_handler500
 
 def hello_world(request):
     return HttpResponse("Hello, world!")
@@ -58,62 +59,16 @@ urlpatterns += [
 ]
 
 
-
-
-
-
-
-
-
-
-
-# customizing default error handlers to return json response instead of html 
-def custom_handler400(request, exception):
-     return JsonResponse({
-        "success": False,
-        "status": 400,
-        "message": "Resource not found",
-        "errors": {
-            "non_field_errors": ["The requested resource was not found."]
-        },
-        "code": "not_found"
-    }, status=400)
-
+# custom basic error handlers
 handler400 = custom_handler400
-
-def custom_handler403(request, exception):
-    return JsonResponse({
-        "success": False,
-        "status": 403,
-        "message": "Forbidden",
-        "errors": {
-            "non_field_errors": ["You do not have permission to perform this action."]
-        },
-        "code": "forbidden"
-    }, status=403)
-
 handler403 = custom_handler403
-
-def custom_handler404(request, exception):
-    return JsonResponse({
-        "success": False,
-        "status": 404,
-        "message": "Resource not found",
-        "errors": {
-            "non_field_errors": ["The requested resource was not found."]
-        },
-        "code": "not_found"
-    }, status=404)
-
 handler404 = custom_handler404
-
-def custom_handler500(request):
-    return JsonResponse({
-        "success": False,
-        "status": 500,
-        "message": "Internal Server Error",
-        "errors": {},
-        "code": "internal_server_error"
-    }, status=500)
-
 handler500 = custom_handler500
+
+
+
+
+
+
+
+
