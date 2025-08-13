b6f34ca60cb61170a75ff355a77a09d79902bbf5
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-08-13 08:01:24 +0600

    feedback permission fixed

--- feedback/views.py
+++ feedback/views.py
@@ -5,7 +5,7 @@ from .permissions import IsAdminOrSelfOrReadOnly
 from users.models import User
 
 class FeedbackViewSet(BaseModelViewSet):
-    queryset = Feedback.objects.all()
+    # queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
     permission_classes = [IsAdminOrSelfOrReadOnly]
     
@@ -14,12 +14,12 @@ class FeedbackViewSet(BaseModelViewSet):
             return Feedback.objects.all()
         if self.request.user.role == User.STAFF:
             return Feedback.objects.filter(fitness_class__instructor=self.request.user)
-        return Feedback.objects.all()
+        return Feedback.objects.filter()
     
-    def get_object(self):
-        if self.request.user.role == User.ADMIN:
-            return super().get_object()
-        return Feedback.objects.get(member=self.request.user)
+    # def get_object(self):
+    #     if self.request.user.role == User.ADMIN:
+    #         return super().get_object()
+    #     return Feedback.objects.get(member=self.request.user)
     
     def perform_create(self, serializer):
         serializer.save(member=self.request.user) 
