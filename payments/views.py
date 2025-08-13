e5a5f25e18ebb9d49acfdf93dddea2c79bf88c8c
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-08-13 06:15:59 +0600

    payments paid_at field added

--- payments/views.py
+++ payments/views.py
@@ -8,6 +8,7 @@ from rest_framework import status
 from core.utils.api_response import error_response
 from core.permissions import IsAdminOrSelf
 import uuid
+from django.utils import timezone
 
 class InvoiceViewSet(BaseModelViewSet):
     # queryset = Invoice.objects.all()
@@ -21,13 +22,13 @@ class InvoiceViewSet(BaseModelViewSet):
 
     def perform_create(self, serializer):
 
-        # payment_type=self.request.query_params.get('subscription')
+        payment_type=self.request.query_params.get('payment_type')
         
-        # if payment_type == 'subscription':
-        # else:
-        #     return error_response('Please provide payment type', status_code=status.HTTP_400_BAD_REQUEST)
-        subscription_data = Subscription.objects.get(member=self.request.user)
-        serializer.save(member=self.request.user, total_cents=subscription_data.plan.price_cents, metadata={"subscription": str(subscription_data.plan)})
+        if payment_type == 'subscription':
+            subscription_data = Subscription.objects.get(member=self.request.user)
+            serializer.save(member=self.request.user, total_cents=subscription_data.plan.price_cents, metadata={"subscription": str(subscription_data.plan)})
+        else:
+            serializer.save(member=self.request.user, total_cents=0, metadata={"subscription": "Empty Invoice"})
 
 
 
@@ -40,10 +41,22 @@ class PaymentViewSet(BaseModelViewSet):
     
     def perform_create(self, serializer):
         invoice = Invoice.objects.get(id=self.request.data.get('invoice'))
-        print("invoice",invoice)
-        invoice.status = 'PAID'
-        invoice.save()
-        serializer.save(member=self.request.user,amount_cents=invoice.total_cents,status='PAID',reference=str(uuid.uuid4()),metadata={"invoice": str(invoice.number)})
+        if invoice.status == 'PAID':
+            return error_response('Invoice already paid', status_code=status.HTTP_400_BAD_REQUEST)
+        if invoice.total_cents > self.request.data.get('amount_cents'):
+            return error_response('Amount is less than invoice total', status_code=status.HTTP_400_BAD_REQUEST)
+        if invoice.total_cents < self.request.data.get('amount_cents'):
+            return error_response('Amount is more than invoice total', status_code=status.HTTP_400_BAD_REQUEST)
+
+        if invoice.total_cents <=0:
+            return error_response('Invoice total is less than or equal to 0', status_code=status.HTTP_400_BAD_REQUEST)
+
+        if invoice.total_cents == self.request.data.get('amount_cents'):
+            invoice.status = 'PAID'
+            invoice.save()
+            serializer.save(member=self.request.user,amount_cents=invoice.total_cents,status='PAID',reference=str(uuid.uuid4()),paid_at=timezone.now(),metadata={"invoice": str(invoice.number)})
+        else:
+            serializer.save(member=self.request.user,amount_cents=invoice.total_cents,status='FAILED',reference=str(uuid.uuid4()),paid_at=timezone.now(),metadata={"invoice": str(invoice.number),"subscription": str(invoice.metadata.get('subscription'))})
     
     def get_queryset(self):
         if self.request.user.role == User.ADMIN or self.request.user.role == User.STAFF:
