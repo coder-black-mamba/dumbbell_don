083f9cd1d5cdab9830cadae0895628f7e12b0ff2
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-08-13 14:27:06 +0600

    highest rating value was '9223372036854776000' fixed to 10

--- feedback/models.py
+++ feedback/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 class Feedback(models.Model):
     member = models.ForeignKey('users.User', on_delete=models.CASCADE)
     fitness_class = models.ForeignKey('classes.FitnessClass', on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField()
+    rating = models.PositiveSmallIntegerField(
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(0)
+        ])
     comment = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
