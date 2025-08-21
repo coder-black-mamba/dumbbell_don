a3129b5f2c086edfa3305fabb40fc9e42459014f
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-08-22 05:37:45 +0600

    permission issue fixed

--- users/permissions.py
+++ users/permissions.py
@@ -4,7 +4,7 @@ from users.models import User
 class IsAdminOrStaffReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
-            if request.user and (request.user.role == User.ADMIN or request.user.role == User.STAFF):
+            if request.user  and (request.user.role == User.ADMIN or request.user.role == User.STAFF):
                 return True
             return False    
         return request.user and request.user.role == User.ADMIN 
