1d2329aea4ade780177ffa1f71bc99e207901de2
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-09-20 09:58:14 +0600

    plan name feature added

--- subscriptions/serializers.py
+++ subscriptions/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import Subscription
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan_title = serializers.CharField(source='plan.name', read_only=True)
+    
     class Meta:
         model = Subscription
-        fields = ['id','member','plan', 'start_date', 'end_date', 'status', 'auto_renew']
-        read_only_fields = ['id', 'start_date','status', 'end_date', 'auto_renew', 'member']
+        fields = ['id', 'member', 'plan', 'plan_title', 'start_date', 'end_date', 'status', 'auto_renew']
+        read_only_fields = ['id', 'start_date', 'status', 'end_date', 'auto_renew', 'member', 'plan_title']
